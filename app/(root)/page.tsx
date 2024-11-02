3f033a36acfb868691f209a994152e184d0bd3b9
ujjwal <ujjwalbaranwal221715@gmail.com>
2024-11-03 00:45:00 +0530

    footer is made

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,23 @@
 import { Button } from "@/components/ui/button";
+import Footer from "@/components/ui/footer";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center flex-col min-h-screen">
-      <h1 className="font-bold text-violet-700 text-4xl pb-3 tracking-wide">ML4E</h1>
-      <p className="font-light text-2xl">
-        Machine Learning for Everyone is a collection of resources to help you learn machine learning.
-      </p>
-      <Link href="/learn">
-        <Button className="mt-5">
-          Start Learning
-        </Button>
-      </Link>
-    </main>
+    <>
+      <main className="flex justify-center items-center flex-col min-h-screen">
+        <h1 className="font-bold text-violet-700 text-4xl pb-3 tracking-wide">
+          ML4E
+        </h1>
+        <p className="font-light text-2xl">
+          Machine Learning for Everyone is a collection of resources to help you
+          learn machine learning.
+        </p>
+        <Link href="/learn">
+          <Button className="mt-5">Start Learning</Button>
+        </Link>
+      </main>
+      <Footer />
+    </>
   );
 }
